c493342aa5f255769e9870a10fa3e68d8eaaff8f
Omar Ashour <68746440+OmarAshour02@users.noreply.github.com>
2024-02-28 17:35:18 +0200

    Revert " * ther explaination of the cors lines :"

--- src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
+++ src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
@@ -1,16 +1,10 @@
 package com.example.cloud_tracker.configuration;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,21 +12,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.securityContext((context) -> context
-                        .requireExplicitSave(false))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-                    @Override
-                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                        CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("*"));
-                        config.setAllowedMethods(Collections.singletonList("*"));
-                        config.setAllowCredentials(true);
-                        config.setAllowedHeaders(Collections.singletonList("*"));
-                        config.setMaxAge(3600L);
-                        return config;
-                    }
-                }))
+        http
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll()
                 );
