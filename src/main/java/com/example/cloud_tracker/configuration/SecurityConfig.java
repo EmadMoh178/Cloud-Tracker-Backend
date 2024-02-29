44369d49b7689460c7b7380d11bc4cff8856cfa0
Mostafa Mahmoud <mostafa19500mahmoud@gmail.com>
2024-02-29 15:09:05 +0200

    *Adding CSRF Configuration : 1 - first we make a filter that will return the csrf token with each response 2 - we use a class ( CsrfTokenRequestAttributeHandler ) to handle our csrf tokens 3 - we ignore the public routes we have 4 - we let the browser to save the cookies from the response 5 - the front end will put the header ( X-XSRF-TOKEN ) with each request

--- src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
+++ src/main/java/com/example/cloud_tracker/configuration/SecurityConfig.java
@@ -5,11 +5,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import java.util.Collections;
+
+import com.example.cloud_tracker.filter.CsrfCookieFilter;
+
+/*
+ * the explaination of the crsf and how it work is : 
+ * we have a public endpoint /hello that we want to ignore the csrf token for it (we can add more endpoints to ignore the csrf token for it)
+ * we have a filter that will add the csrf token to the response header
+ * and the csrf token will be added to the cookie and the response header
+ * and every request will be checked for the csrf token and if it is not there it will return 403 
+ * we add the csrf token to the request header in the form of X-XSRF-TOKEN = csrf token
+ */
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +31,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors(cors -> cors.configurationSource(new CorsConfig()))
+        
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+
+        http
+        .cors(cors -> cors.configurationSource(new CorsConfig()))
+
+        // i put the csrf config in the csrfconfig class and i will call it here
+        .csrf((csrf) -> csrf.getClass().equals(CsrfConfig.class))
+
         .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll());
         return http.build();
--- src/main/java/com/example/cloud_tracker/controller/Home.java
+++ src/main/java/com/example/cloud_tracker/controller/Home.java
@@ -2,13 +2,20 @@ package com.example.cloud_tracker.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Home {
     @GetMapping("/")
     public String home() {
         return "Hello, World!";
+    }
+
+    @PostMapping("/hello")
+    public String postMethodName(@RequestParam String text) {
+        return text;
     }
 
 }
