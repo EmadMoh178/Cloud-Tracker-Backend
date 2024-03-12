d1368ee1a8b0b96ab64886d5cf2a120856636070
Omar Ashour <omarashour02@gmail.com>
2024-03-12 15:18:02 +0200

    Code Refactor
    - Changed the datatypes from "Optional<User>" to "User"
    - Changed "Role" m"IAMRole"
    - Changed generic RespsponseEntity<Object specific returned object

--- src/main/java/com/example/cloud_tracker/model/IAMRole.java
+++ src/main/java/com/example/cloud_tracker/model/IAMRole.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "roles")
-public class Role {
+public class IAMRole {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
